b87a5f242f3b77e2a05d142305ca16b825562a95
Cristian Meira <cristian.meira15@gmail.com>
2026-01-29 16:52:41 -0300

    Update filas.c
    Adicionei as funções: InicializarFila e SomaTermosFila

--- filas.c
+++ filas.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "filas.h"
+
+void InicializarFila(FILA *f){
+    f->inicio = NULL;
+    f->fim = NULL;
+}
+
+int SomaTermosFila(FILA *f){
+    if(!f->inicio) return 0;
+    NOF* p = f->inicio;
+    int soma = 0;
+    while(p){
+        soma += p->info;
+        p = p->prox;
+    }
+    return soma;
+}
+
 void menuFilas() { printf("Menu Filas em breve...\n"); }
